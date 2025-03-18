c7b991214982888e6c2eb3a82a06dd8bc5b8e06a
cpigache <cpigache@myges.fr>
2025-03-18 21:48:26 +0100

    mushrooms decors

---
+++ Assets/script/script camera.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class scriptcamera : MonoBehaviour
+{
+    public GameObject Player;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position;
+    }
+}
